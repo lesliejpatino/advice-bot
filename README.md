97a47376618dd985560bf61bb17b3d9fc645b23f
Leslie Patino <lesliejpatino@gmail.com>
2022-05-05 16:15:46 -0700

    created second fetch request that will allow user to get more advice and added temporary background.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# quick-advice
+# Advice Bot
+
+# Description
+
+# Table of Contents
+* [Description](#description)
+* [Demo & Site Link](#demo--site-link)
+* [Technologies Used]()
+* [Author](#author)
+* [License](#license)
+
+# Demo & Site Link
+* [Advice Bot - Live Site](https://lesliejpatino.github.io/advice-bot/)
+
+# Technologies Used
+* [Advice Slip Api](https://api.adviceslip.com/#top)
+* [Bootstrap](https://getbootstrap.com/)
+* Javascript
+* CSS
+* HTML
+
+# Author
+**[Leslie Patino](https://lesliejpatino.github.io/portfolio/)**
+
+* [Leslie's LinkedIn](https://www.linkedin.com/in/lesliejpatino/)
+* [Leslie's GitHub](https://github.com/lesliejpatino)
+
+## License
+Copyright (c) 2022 Leslie Patino <br />
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) <br/>
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const adviceBtn = document.getElementById('advice-btn');
+const moreAdviceBtn = document.getElementById('more-advice')
 const robotIntro = document.getElementById('robot-intro');
 const responseDiv = document.getElementById('response-div')
 const robotAdvice = document.getElementById('robot-advice');
+
 
 const getAdvice = function() {
     const adviceUrl = 'https://api.adviceslip.com/advice';
@@ -20,7 +22,27 @@ const getAdvice = function() {
         })
 };
 
+const moreAdvice = function() {
+    const adviceUrl = 'https://api.adviceslip.com/advice';
+
+    fetch(adviceUrl)
+        .then(function (response) {
+            console.log("response", response)
+            return response.json();
+        })
+        .then(function (data) {
+            // data.slip.advice = 1 piece of advice
+            console.log(data.slip.advice);
+            // robotIntro.style.display= "none"
+            // responseDiv.style.display= "block";
+            robotAdvice.textContent = data.slip.advice
+        })
+
+}
+
 // addEventListener method is only available on an individual element
 adviceBtn.addEventListener('click', getAdvice)
+moreAdviceBtn.addEventListener('click', moreAdvice)
+
 
 console.log('testing')
