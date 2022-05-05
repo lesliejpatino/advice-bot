d44c26a08ce4191f2b9fc126004094b5f3314825
Leslie Patino <lesliejpatino@gmail.com>
2022-05-05 14:11:34 -0700

    created fetch request that displays 1 random piece of advice

---
+++ script.js
@@ -0,0 +1,26 @@
+const adviceBtn = document.getElementById('advice-btn');
+const robotIntro = document.getElementById('robot-intro');
+const responseDiv = document.getElementById('response-div')
+const robotAdvice = document.getElementById('robot-advice');
+
+const getAdvice = function() {
+    const adviceUrl = 'https://api.adviceslip.com/advice';
+
+    fetch(adviceUrl)
+        .then(function (response) {
+            console.log("response", response)
+            return response.json();
+        })
+        .then(function (data) {
+            // data.slip.advice = 1 piece of advice
+            console.log(data.slip.advice);
+            robotIntro.style.display= "none"
+            responseDiv.style.display= "block";
+            robotAdvice.textContent = data.slip.advice
+        })
+};
+
+// addEventListener method is only available on an individual element
+adviceBtn.addEventListener('click', getAdvice)
+
+console.log('testing')
